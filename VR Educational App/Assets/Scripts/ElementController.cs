4437028d2d343f3ac2d44d63065aa9d195798977
sshook98 <sscheckmate1@gmail.com>
2020-03-11 20:48:42 -0400

    Element Controller Script

---
+++ VR Educational App/Assets/Scripts/ElementController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementController : MonoBehaviour
+{
+    public bool isFlammable = true;
+    public bool conductsElectricity = true;
+    public bool isMagnetic = true;
+    public bool isSoluble = true;
+    public float mass = 1f;
+    public float volume = 1f;
+
+    public float GetDensity()
+    {
+        return mass / volume;
+    }
+}
