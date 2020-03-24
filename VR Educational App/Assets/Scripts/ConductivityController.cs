1ec48fc8e256f37bed977d40fb6de3df66ddd87d
dsaiontz <david.saiontz@gmail.com>
2020-03-24 14:48:44 -0400

    Add Functionality to Conductivity Box

---
+++ VR Educational App/Assets/Scripts/ConductivityController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConductivityController : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void onTriggerEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+        if (other.tag.Equals("Element"))
+        {
+            ElementController ec = other.GetComponent<ElementController>();
+
+            if (ec.ConductsElectricity()) {
+                GetComponent<ParticleSystem>().Play();
+                ParticleSystem.EmissionModule em = GetComponent<ParticleSystem>().emission;
+                em.enabled = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        GameObject other = collision.gameObject;
+        if (other.tag.Equals("Element"))
+        {
+            ElementController ec = other.GetComponent<ElementController>();
+
+            if (ec.ConductsElectricity())
+            {
+                GetComponent<ParticleSystem>().Stop();
+                ParticleSystem.EmissionModule em = GetComponent<ParticleSystem>().emission;
+                em.enabled = false;
+            }
+        }
+    }
+}
